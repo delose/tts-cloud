da7ebf9e39949daabca4e404e4003d86c40443a9
Gio <delossantos.eugene@gmail.com>
2021-11-20 14:46:29 +0800

    init

---
+++ tts-cloud/src/main/java/com/delose/ttscloud/TtsCloudApplication.java
@@ -0,0 +1,13 @@
+package com.delose.ttscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsCloudApplication.class, args);
+	}
+
+}
---
+++ tts-cloud/src/test/java/com/delose/ttscloud/TtsCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delose.ttscloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
