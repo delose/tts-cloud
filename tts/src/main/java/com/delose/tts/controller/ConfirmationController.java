3c2cd135d3d5f45323cb677ffecbb7786383da97
Gio <delossantos.eugene@gmail.com>
2021-11-27 21:59:52 +0800

    mavigate using buttons

--- tts/src/main/java/com/delose/tts/controller/ConfirmationController.java
+++ tts/src/main/java/com/delose/tts/controller/ConfirmationController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -15,5 +16,10 @@ public class ConfirmationController {
     public String save(Model model) {
         // TODO: add attribute
         return "saved";
+    }
+
+    @PostMapping("/go-back")
+    public String goBackHome() {
+        return "redirect:/";
     }
 }
--- tts/src/main/java/com/delose/tts/controller/HomeController.java
+++ tts/src/main/java/com/delose/tts/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.delose.tts.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -12,5 +13,10 @@ public class HomeController {
     public String home() {
         log.info("display home");
         return "home";
+    }
+
+    @PostMapping("/go-to-add-article")
+    public String goToAddArticle() {
+        return "redirect:/article";
     }
 }
