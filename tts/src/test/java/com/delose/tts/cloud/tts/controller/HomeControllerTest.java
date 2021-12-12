90c777657f202764a79a22b7a95103f294f39708
Gio <delossantos.eugene@gmail.com>
2021-12-12 16:40:11 +0800

    failing test [fix]

--- tts/src/test/java/com/delose/tts/cloud/tts/controller/HomeControllerTest.java
+++ tts/src/test/java/com/delose/tts/cloud/tts/controller/HomeControllerTest.java
@@ -1,14 +1,20 @@
 package com.delose.tts.cloud.tts.controller;
 
+import com.delose.tts.cloud.tts.repository.ArticleRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -17,8 +23,14 @@ public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ArticleRepository articleRepository;
+
     @Test
     public void testHomePage() throws Exception {
+        articleRepository = Mockito.mock(ArticleRepository.class);
+
+        when(articleRepository.findAll()).thenReturn(new ArrayList<>());
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
