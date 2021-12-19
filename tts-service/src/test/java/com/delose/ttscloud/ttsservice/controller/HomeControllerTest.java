b8578760aa33f3cefc1b8314da9988696d4ad958
Gio <delossantos.eugene@gmail.com>
2021-12-19 19:31:45 +0800

    uncomment junit vintage

--- tts-service/src/test/java/com/delose/ttscloud/ttsservice/controller/HomeControllerTest.java
+++ tts-service/src/test/java/com/delose/ttscloud/ttsservice/controller/HomeControllerTest.java
@@ -1,11 +1,13 @@
 package com.delose.ttscloud.ttsservice.controller;
 
 import com.delose.ttscloud.ttsservice.repository.ArticleRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -15,7 +17,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
     @Autowired
